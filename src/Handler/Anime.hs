4aab8063ca2d4ab5191f49535ff3d3932212a16b
Jonathan Araujo 3 <jnh.araujo@outlook.com>
2018-12-12 00:08:27 +0000

    Listagem de animes - api

--- src/Handler/Anime.hs
+++ src/Handler/Anime.hs
@@ -9,8 +9,18 @@ import Import
 
 postAnimeR :: Handler TypedContent
 postAnimeR = do
+    addHeader "Access-Control-Allow-Origin" "*"
     anime <- requireJsonBody :: Handler Anime
     animeid <- runDB $ insert anime
     sendStatusJSON created201 (object ["resp" .= animeid])
     
--- curl -X POST https://yesodia2-wickedjhow.c9users.io/anime -d '{"titulo":"Boruto","descricao":"Naruto só que pior","imagem":"nao","genero":"shonnen"}'
+-- curl -X POST https://yesodia2-wickedjhow.c9users.io/anime -d '{"titulo":"Jojo's Bizzare Adventure","descricao":"Pancadaria Solta","imagem":"http://www.animationmagazine.net/wordpress/wp-content/uploads/jojos-bizarre-adventure-post2-1.jpg","genero":"acao"}'
+
+getAnimesR :: Handler TypedContent
+getAnimesR = do
+    addHeader "Access-Control-Allow-Origin" "*"
+    animes <- runDB $ selectList [] [Asc AnimeTitulo]
+    sendStatusJSON ok200 (object ["resp" .= animes]) 
+    
+-- curl https://yesodia2-wickedjhow.c9users.io/animes 
+
