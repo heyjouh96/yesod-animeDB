0f7d392d6998f4beb7f6fb02b1889923c288b046
Gabriel Craveiro <gabriel.craveiro@intuiti.com.br>
2018-12-13 12:35:32 -0200

    Cadastro de animes funcionando

--- webapp/anime-db-webapp/src/views/Cadastro.vue
+++ webapp/anime-db-webapp/src/views/Cadastro.vue
@@ -1,9 +1,9 @@
 <template>
-    <section class="fdb-block" style="background-image: url(https://cdn.jsdelivr.net/gh/froala/design-blocks@2.0.1/dist/imgs//shapes/1.svg);" data-block-type="forms" data-id="7" draggable="true">
+<section class="fdb-block" style="background-image: url(https://cdn.jsdelivr.net/gh/froala/design-blocks@2.0.1/dist/imgs//shapes/1.svg);" data-block-type="forms" data-id="7" draggable="true">
   <div class="container">
     <div class="row justify-content-end">
       <div class="col-12 col-md-9 col-lg-8">
-                <div class="fdb-box fdb-touch">
+        <div class="fdb-box fdb-touch">
           <div class="row">
             <div class="col">
               <h1>Cadastrar anime</h1>
@@ -13,43 +13,43 @@
             <div class="col mt-4">
               <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Titulo">
             </div>
-          </div>
-          <div class="row mt-4">
-            <div class="col">
-              <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Descricao">
             </div>
-          </div>
-          
-          <div class="row mt-4">
+            <div class="row mt-4">
               <div class="col">
-            <label for="exampleFormControlSelect1">Genero</label>
-            <select class="form-control" id="genero">
+                <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Descricao">
+            </div>
+              </div>
+
+              <div class="row mt-4">
+                <div class="col">
+                  <label for="exampleFormControlSelect1">Genero</label>
+                  <select class="form-control" id="genero">
               <option  v-for="(item, index) in generos" :key='index' :value="item.id">{{ item.descricao }}</option>
             </select>
-          </div>
-          </div>
-          
-          <div class="row mt-4">
-            <div class="col">
-              <input type="text" class="form-control" id="imagem" name="imagem" placeholder="Imagem">
+                </div>
+              </div>
+
+              <div class="row mt-4">
+                <div class="col">
+                  <input type="text" class="form-control" id="imagem" name="imagem" placeholder="Imagem">
             </div>
-          </div>
-          <div class="row mt-4">
-            <div class="col">
-              <button class="btn btn-primary" type="button" @click="postAnime">Cadastrar</button>
+                </div>
+                <div class="row mt-4">
+                  <div class="col">
+                    <button class="btn btn-primary" type="button" @click="postAnime">Cadastrar</button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  </div>
 </section>
 </template>
 
 <script>
 import axios from 'axios';
 import qs from 'qs'
-
+import $ from 'jquery';
 
 export default {
   name: "Consulta",
@@ -59,57 +59,47 @@ export default {
     }
   },
   computed: {},
-  mounted () {
+  mounted() {
     this.getList();
-   
+
   },
   methods: {
-    getList: function() {
+    getList: function () {
       axios
-      .get('https://yesodia2-wickedjhow.c9users.io/anime/generos')
-      .then(response => (this.generos = response.data.resp))
-      .then(console.log(this.generos)) 
+        .get('https://yesodia2-wickedjhow.c9users.io/anime/generos')
+        .then(response => (this.generos = response.data.resp))
+        .then(console.log(this.generos))
     },
-    
-    postAnime: function() {
-      
+
+    postAnime: function () {
+
       let titulo = document.getElementById("titulo").value;
       let descricao = document.getElementById("descricao").value;
       let imagem = document.getElementById("imagem").value;
       let genero = document.getElementById("genero").value;
 
-      let data = {
-        titulo: "titulo",
-        descricao: "descricao",
-        imagem: "imagem",
+      var data = {
+        titulo: titulo,
+        descricao: descricao,
+        imagem: imagem,
         generoid: 1,
       };
+      
 
-      // axios.post('https://yesodia2-wickedjhow.c9users.io/anime', qs.stringify(data))
+      console.log(data);
 
-      // url = 'https://yesodia2-wickedjhow.c9users.io/anime';
+      var fd = $.ajax({
+        url: "https://yesodia2-wickedjhow.c9users.io/anime",
+        type: "POST",
+        data: JSON.stringify(data)
+      });
 
-      // const options = {
-      //   method: 'POST',
-      //   headers: { 'content-type': 'application/x-www-form-urlencoded', "Access-Control-Allow-Origin": "*" },
-      //   data: qs.stringify(data),
-      //   url: 'https://yesodia2-wickedjhow.c9users.io/anime',
-      // };
-      // axios(options);
-
-      this.$http.post('https://yesodia2-wickedjhow.c9users.io/anime', {titulo: "titulo",
-        descricao: "descricao",
-        imagem: "imagem",
-        generoid: 1}).then    (function (response) {
-            // Success
-            console.log(response.data)
-        },function (response) {
-            // Error
-            console.log(response.data)
-        });
-            }
-          },
-        };
+      fd.done(function(resp) {
+        console.log(resp);
+      })
+    }
+  }
+}
 </script>
 
 <style lang="scss">
