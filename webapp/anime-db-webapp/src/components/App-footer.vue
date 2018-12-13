68e4ef0700498bab69cceb2616e7285e31dfbbf4
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2018-12-13 00:25:53 +0000

    Inclusão footer

--- webapp/anime-db-webapp/src/components/App-footer.vue
+++ webapp/anime-db-webapp/src/components/App-footer.vue
@@ -1,4 +1,23 @@
 <template>
+  <footer class="fdb-block footer-small bg-dark" data-block-type="footers" data-id="10">
+  <div class="container">
+    <div class="row text-center align-items-center">
+      <div class="col">
+        <ul class="nav justify-content-center">
+          <li class="nav-item">
+            <a class="nav-link active" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://github.com/heyjouh96/yesod-animeDB">Github</a>
+          </li>
+        </ul>
+
+        <p class="h5 mt-5">© 2018 - Gabriel - Leo - Jhon</p>
+      </div>
+    </div>
+  </div>
+</footer>
+  
 </template>
 
 <script>
