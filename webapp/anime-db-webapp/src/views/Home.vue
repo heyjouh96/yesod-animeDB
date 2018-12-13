dae3edcfc18fdcedbcf87a266e5b1a1668ec2960
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2018-12-13 00:43:43 +0000

    Inclusão home

--- webapp/anime-db-webapp/src/views/Home.vue
+++ webapp/anime-db-webapp/src/views/Home.vue
@@ -1,7 +1,113 @@
 <template>
-<div class="col-12 col-md-2 container__logo">
-  
-</div>
+  <div>
+    <section class="fdb-block py-0 fp-active" data-block-type="call_to_action" data-id="3" >
+  <div class="container py-5 my-5" style="background-image: url(https://cdn.jsdelivr.net/gh/froala/design-blocks@2.0.1/dist/imgs//shapes/2.svg);">
+    <div class="row justify-content-center py-5">
+      <div class="col-12 col-md-10 col-lg-8 text-center">
+        <div class="fdb-box">
+          <h1>Biblioteca comunitária de animes</h1>
+          <p class="lead">
+            Uma biblioteca e api para consumo e consulta de animes, conheça novos animes e ajude a aumentar nosso catálogo.
+          </p>
+          <p class="mt-4">
+            <a class="btn btn-primary" href="/consulta">Consultar</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="fdb-block" data-block-type="contents" data-id="4" draggable="true">
+  <div class="container">
+    <div class="row">
+      <div class="col text-center">
+        <h1>Uma api feita de usuários para usuários</h1>
+      </div>
+    </div>
+
+    <div class="row pt-5 justify-content-center align-items-center">
+      <div class="col-4">
+        <img alt="image" class="img-fluid" src="https://cdn.jsdelivr.net/gh/froala/design-blocks@2.0.1/dist/imgs//draws/developer.svg">
+      </div>
+      <div class="col-4 offset-1">
+        <img alt="image" class="img-fluid" src="https://cdn.jsdelivr.net/gh/froala/design-blocks@2.0.1/dist/imgs//draws/git.svg">
+      </div>
+    </div>
+  </div>
+</section>
+<section class="fdb-block" data-block-type="features" data-id="6" draggable="true">
+  <div class="container">
+    <div class="row text-center">
+      <div class="col-12 col-sm-6 col-md-3">
+        <img alt="image" class="fdb-icon" src="https://cdn.jsdelivr.net/gh/froala/design-blocks@2.0.1/dist/imgs//icons/monitor.svg">
+        <h3><strong>Consulte animes</strong></h3>
+        <p class="mt-3"><a class="btn btn-primary" href="/consulta">Consulte</a></p>
+      </div>
+
+      <div class="col-12 col-sm-6 col-md-3 pt-5 pt-sm-0">
+        <img alt="image" class="fdb-icon" src="https://cdn.jsdelivr.net/gh/froala/design-blocks@2.0.1/dist/imgs//icons/layout.svg">
+        <h3><strong>Cadastre animes que você recomenda!</strong></h3>
+        <p class="mt-3"><a class="btn btn-primary" href="/cadastrar">Cadastrar</a></p>
+      </div>
+
+      <div class="col-12 col-sm-6 col-md-3 pt-5 pt-md-0">
+        <img alt="image" class="fdb-icon" src="https://cdn.jsdelivr.net/gh/froala/design-blocks@2.0.1/dist/imgs//icons/map-pin.svg">
+        <h3><strong>De nota e ajude seu anime favorito a se destacar</strong></h3>
+        <p class="mt-3"><a class="btn btn-primary" href="/nota">Avaliar</a></p>
+      </div>
+
+      <div class="col-12 col-sm-6 col-md-3 pt-5 pt-md-0">
+        <img alt="image" class="fdb-icon" src="https://cdn.jsdelivr.net/gh/froala/design-blocks@2.0.1/dist/imgs//icons/github.svg">
+        <h3><strong>Atualize informações de animes</strong></h3>
+        <p class="mt-3"><a class="btn btn-primary" href="/atualizar">Atualizar</a></p>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="fdb-block py-5" data-block-type="testimonials" data-id="11" draggable="true">
+  <div class="container py-5" style="background-image: url(https://cdn.jsdelivr.net/gh/froala/design-blocks@2.0.1/dist/imgs//shapes/2.svg);">
+    <div class="row align-items-center justify-content-center">
+      <div class="col-lg-9 col-xl-6">
+        <div class="fdb-box">
+          <div class="row">
+            <div class="col-8 col-sm-6 col-md-4 col-xl-3 ml-auto mr-auto">
+              <img alt="image" class="img-fluid rounded" src="https://vignette.wikia.nocookie.net/dbz-dokkanbattle/images/f/f2/Gohan_studying_at_a_desk.jpg/revision/latest?cb=20170201164239">
+            </div>
+
+            <div class="col-md-8 mt-4 mt-md-0">
+              <p class="lead">
+                "Nunca havia visto uma maneira tão rápida de inserir animes e conhecer cultura."
+              </p>
+
+              <p class="h3 mt-4 mt-xl-5"><strong></strong></p>
+              <p><em>Gohan</em></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-9 col-xl-6 mt-4 mt-xl-0">
+        <div class="fdb-box">
+          <div class="row">
+            <div class="col-8 col-sm-6 col-md-4 col-xl-3 ml-auto mr-auto">
+              <img alt="image" class="img-fluid rounded" src="https://figuya.com/uploads/product/profile_picture/9074/profile_Gundam-converge-sp07-vorschau.jpg20170608-26915-1lm8mrp">
+            </div>
+
+            <div class="col-md-8 mt-4 mt-md-0">
+              <p class="lead">
+                "Agora posso ver e analisar os vilões antes que eles invadam meu universo"
+              </p>
+
+              <p class="h3 mt-4 mt-xl-5"><strong></strong></p>
+              <p><em>Gundam SP Freedom 07</em></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+  </div>
 </template>
 
 <script>
